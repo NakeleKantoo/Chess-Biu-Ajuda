cb07ea10a3f286e90063e84a096ca775c349dabe
dnlKaizer <danilokaizer@gmail.com>
2026-02-15 22:43:06 -0300

    refactor: agora as exceções estendem a classe abstrata ChessException, permitindo tratamento de forma genérica

--- backend/src/main/java/com/chess/domain/exception/player/SelfJoinGameException.java
+++ backend/src/main/java/com/chess/domain/exception/player/SelfJoinGameException.java
@@ -1,17 +1,30 @@
 package com.chess.domain.exception.player;
 
+import java.util.Map;
 import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+
+import com.chess.domain.exception.ChessException;
 
 import lombok.Getter;
 
 @Getter
-public class SelfJoinGameException extends RuntimeException {
+public class SelfJoinGameException extends ChessException {
     
     private final UUID playerId;
 
     public SelfJoinGameException(UUID playerId) {
         super("Jogador não pode entrar em um jogo que ele mesmo criou. (ID: " + playerId + ")");
         this.playerId = playerId;
+    }
+
+    @Override public String getType() { return "Self Join Game"; }
+    @Override public HttpStatus getStatus() { return HttpStatus.BAD_REQUEST; }
+
+    @Override
+    public Map<String, Object> getDetails() {
+        return Map.of("playerId", playerId);
     }
 
 }
