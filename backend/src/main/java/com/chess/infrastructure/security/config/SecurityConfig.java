ad9efde9462a5288afebecde1cb2ecac7b4371c6
dnlKaizer <danilokaizer@gmail.com>
2026-02-17 09:41:26 -0300

    refactor[backend]: colocando o prefixo "api" nas rotas dos controllers e movendo SecurityConfig para o pacote de configuração

--- backend/src/main/java/com/chess/infrastructure/security/config/SecurityConfig.java
+++ backend/src/main/java/com/chess/infrastructure/security/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.chess.infrastructure.security;
+package com.chess.infrastructure.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
