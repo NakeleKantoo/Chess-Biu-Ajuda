4b1fa4764d5cbfa0c9152df6ed6376e528402928
dnlKaizer <danilokaizer@gmail.com>
2026-02-17 09:12:18 -0300

    feat: feat: adicionando rotas e removendo template padrão do app.html

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,10 +2,16 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient()
   ]
+};
+
+export const appSettings = {
+  API_URL: 'http://localhost:8080'
 };
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { Login } from './features/auth/pages/login/login';
+import { Register } from './features/auth/pages/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'register', // Trocar pela home depois
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        component: Login,
+        title: 'Login Page'
+    },
+    {
+        path: 'register',
+        component: Register,
+        title: 'Register Page'
+    },
+    {
+        path: '**',
+        redirectTo: 'register' // Trocar pela home depois
+    }
+];
