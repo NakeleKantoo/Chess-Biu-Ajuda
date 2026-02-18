6c135d5a562cb0cc888b8b370cd81f7d95c0080e
dnlKaizer <danilokaizer@gmail.com>
2026-02-18 14:15:18 -0300

    feat: página base da home com animações e verificação de autenticação

---
+++ frontend/src/app/features/pages/home/home.ts
@@ -0,0 +1,37 @@
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../../core/auth/auth.service';
+import { Router } from '@angular/router';
+import { map, take } from 'rxjs';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.html',
+  styleUrl: './home.scss',
+})
+export class Home {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  createGame() {
+    if (this.authenticated()) {
+      console.log('Creating game...');
+    }
+  }
+
+  enterGame() {
+    if (this.authenticated()) {
+      console.log('Entering game...');
+    }
+  }
+
+  authenticated() : boolean {
+    const isAuthenticated = this.authService.isAuthenticated;
+    if (!isAuthenticated) {
+      this.router.navigate(['/login']);
+      return false;
+    }
+    return true;
+  }
+
+}
