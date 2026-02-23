5bea80771e0d862054c6e0419f428fb9b41b16e4
dnlKaizer <danilokaizer@gmail.com>
2026-02-23 10:40:43 -0300

    feat: método validateToken no AuthService e o AuthInterceptor agora faz logout e redireciona para a página de login se o token não estiver salvo

--- frontend/src/app/core/auth/auth.interceptor.ts
+++ frontend/src/app/core/auth/auth.interceptor.ts
@@ -1,18 +1,28 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from './auth.service';
+import { catchError, throwError } from 'rxjs';
+import { Router } from '@angular/router';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
     const authService = inject(AuthService);
+    const router = inject(Router);
     const token = authService.getToken();
 
+    let authReq = req;
     if (token) {
-        req = req.clone({
-            setHeaders: {
-                Authorization: `Bearer ${token}`
-            }
+        authReq = req.clone({
+            setHeaders: { Authorization: `Bearer ${token}` }
         });
     }
 
-    return next(req);
+    return next(authReq).pipe(
+        catchError((error: HttpErrorResponse) => {
+            if (error.status === 401) {
+                authService.logout();
+                router.navigate(['/login']);
+            }
+            return throwError(() => error);
+        })
+    );
 };
